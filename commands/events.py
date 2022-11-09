bdad04778b17d34ee784ea2dc0e0c2bcc71194a8
TheTw3ys <44274116+TheTw3ys@users.noreply.github.com>
2022-11-09 11:20:50 +0100

    Initial commit of Member leave and join
    probably contains errors

---
+++ commands/events.py
@@ -0,0 +1,45 @@
+import lightbulb
+from hikari.permissions import Permissions
+import sys
+import os
+os.chdir(os.getcwd() + "/storage")
+
+sys.path.append(f"{os.getcwd()}")
+
+from functions_and_classes import Administration
+
+plugin = lightbulb.Plugin(name="StupidStuff", description="Some things I came up with dunno")
+
+
+@plugin.listener(event=hikari.MemberCreateEvent)
+async def on_member_join(event: hikari.MemberCreateEvent):
+    guild = await event.app.rest.fetch_guild(event.message.guild_id): hikari.GatewayGuild
+    guild_json = str(os.path.join(os.getcwd(), "ranking", (guild.name + ".json")))
+    member = event.member
+    with open(guild_json, "r") as f:
+        users = json.load(f)
+        for user in users:
+            if f'{user.id}' == user:
+                #greet old member
+            else:
+                #greet new member 
+        
+@plugin.listener(event=hikari.MemberDeleteEvent)
+async def on_member_join(event: hikari.MemberCreateEvent):
+    member = event.member
+	await event.app.rest.create_message(channel=event.channel_id,
+                                        content=f'{member.name} has left')
+
+
+
+
+
+def load(bot: lightbulb.BotApp):
+    bot.add_plugin(plugin)
+
+
+def unload(bot: lightbulb.BotApp):
+    bot.remove_plugin(plugin)
+
+
+os.chdir("..")
