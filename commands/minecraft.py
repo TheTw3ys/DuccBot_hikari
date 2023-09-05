96e2ae835343c5a293470a5d7f0de33bbb13c2e2
TheTw3ys <44274116+TheTw3ys@users.noreply.github.com>
2023-09-05 17:04:59 +0000

    fixed typo

--- commands/minecraft.py
+++ commands/minecraft.py
@@ -17,7 +17,7 @@ guild_list = [699010600331771955, 911288030210428938, 715208493237403731]
 @lightbulb.option(name="ign", type=str, description="your Ingame Name", required=False)
 @lightbulb.command(name="uuid", description="Gives you your UUID")
 @lightbulb.implements(lightbulb.commands.PrefixCommand)
-async def command_uuid(ctx: lightbulb.context.PrefixContext):
+async def command_ign(ctx: lightbulb.context.PrefixContext):
     ign = ctx.options.ign.strip()
     response = requests.request(url=f"https://minecraft-api.com/api/uuid/{ign}",method= "GET", timeout=20)
     await ctx.respond(hikari.Embed(description=response.text, color=0x22a7f0 ))
